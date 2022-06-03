b771e981c7ef89ce1f8c5c8bc65159577ea44e3d
Rafał Kuśmierz <r.kusmierz@stud.elka.pw.edu.pl>
2022-06-03 20:25:34 +0200

    ciekawe czy zadziala

--- makeSeriesByWeight.py
+++ makeSeriesByWeight.py
@@ -12,7 +12,7 @@ products_df = pd.read_json("data/products.jsonl", lines=True)
 # sessions_df = sessions_df.loc[sessions_df["event_type"] == "RETURN_PRODUCT"]
 df = sessions_df.merge(products_df, on="product_id", how="left")
 
-def make_count_prod_weight_by_weeks(df, products_dict):
+def make_count_prod_weight_by_weeks(df, products_dict, keys):
     dates = df.values.tolist()
     # print([i for i in dates if i[3] <= 10 and i[3] > 3])
 
@@ -25,28 +25,23 @@ def make_count_prod_weight_by_weeks(df, products_dict):
         }
         products_dict[key] = cups
 
+
     for event in dates:
-        if 0 < event[3] <= 0.2:
-            products_dict['0-0.2'][str(event[1])][event[2]] += 1
-        elif 0.2 < event[3] <= 0.5:
-            products_dict['0.2-0.5'][str(event[1])][event[2]] += 1
-        elif 0.5 < event[3] <= 1:
-            products_dict['0.5-1'][str(event[1])][event[2]] += 1
-        elif 1 < event[3] <= 3:
-            products_dict['1-3'][str(event[1])][event[2]] += 1
-        elif 3 < event[3] <= 10:
-            products_dict['3-10'][str(event[1])][event[2]] += 1
-        elif 10 < event[3] <= 30:
-            products_dict['10-30'][str(event[1])][event[2]] += 1
+        for key in keys:
+            down = float(key.split('-')[0])
+            up = float(key.split('-')[1])
+            if down < event[3] <= up:
+                products_dict[key][str(event[1])][event[2]] += 1
+
 
     tables_for_plt = []
 
     for key in products_dict:
         ex = make_four_weeks_cups(products_dict[key])
         tables_for_plt.append(ex)
-    #     plt.plot(ex)
-    #     plt.xlabel(key)
-    #     plt.show()
+        # plt.plot(ex)
+        # plt.xlabel(key)
+        # plt.show()
     # print(tables_for_plt)
     return tables_for_plt
 
@@ -59,4 +54,3 @@ if __name__ == "__main__":
     products_dict = dict.fromkeys(keys)
 
     tables_for_plot = make_count_prod_weight_by_weeks(df, products_dict)
-
