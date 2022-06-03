c32f4497f0c8017f3dfb14c4b724c38cfd865c16
Rafał Kuśmierz <r.kusmierz@stud.elka.pw.edu.pl>
2022-06-03 12:12:18 +0200

    server pickle model

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
 from flask import Flask, jsonify, request
+#from makeComplexPrediction import *
+from makePrediction import *
+import pickle
+import datetime
 
 app = Flask(__name__)
 
@@ -14,6 +18,10 @@ def get_tasks():
     request_data = request.get_json()
 
     # do some kind of prediction
+    model = pickle.load(open('make_prediction_model.sav', 'wb'))
+    date = datetime.datetime(2022, 2, 15)
+    weeks = weeksBefore(date, 4)
+    predict(weeks, model)
     predicted_number_of_returns = 10
     return f"Predicted number of returns for {request_data['day']} day of {request_data['month']} {request_data['year']} is {predicted_number_of_returns}"
 
